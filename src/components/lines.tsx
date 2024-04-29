c561ddb7649f259104306f0d0973c12386cab8dc
shkarDiary <95171810+shkarDiary@users.noreply.github.com>
2024-04-29 15:13:05 +0300

    correct the overflow

--- src/components/lines.tsx
+++ src/components/lines.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Lines() {
   return (
-    <div className="h-dvh w-screen flex justify-evenly bg-transparent absolute top-0 -z-10 ">
+    <div className="h-dvh w-full flex justify-evenly bg-transparent absolute  -z-10 ">
       <span className=" h-full w-[1px] bg-gray-300 " />
       <span className=" h-full w-[1px] bg-gray-300 " />
       <span className=" h-full w-[1px] bg-gray-300 " />
