f1215398fed8661aa3962b9a0882c4663887548b
shkarDiary <95171810+shkarDiary@users.noreply.github.com>
2024-05-01 10:31:40 +0300

    initial code for swiper

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -2,9 +2,14 @@
 import Image from "next/image";
 import Nextbtn from "@mui/icons-material/ArrowForward";
 import Prevbtn from "@mui/icons-material/ArrowBack";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { MouseEventHandler } from "react";
 export default function Slider() {
   const [number, setNumber] = useState(0);
+  const [startX, setStartX] = useState(null);
+  const [widthRoot, setWidthRoot] = useState(0);
+  const [translateX, setTranslateX] = useState(0);
+  const itemRef = useRef(null);
   const works = [
     { name: "kp1", url: "/works/kp1.jpg" },
     { name: "kp2", url: "/works/kp2.jpg" },
@@ -15,6 +20,17 @@ export default function Slider() {
     { name: "manipulation", url: "/works/manipulation.png" },
     { name: "logo", url: "/works/logo.jpg" },
   ];
+  useEffect(() => {
+    if (number * 224 > (works.length - 3) * 224) setNumber(0);
+    else if (number < 0) setNumber(works.length - 3);
+  }, [number]);
+  useEffect(() => {
+    const getWidthItems = () => {
+      if (itemRef.current) {
+        const itemRefRect = itemRef.current.clientWidth;
+      }
+    };
+  }, []);
   const nextbtnHandler = () => {
     number < works.length ? console.log("continue") : console.log("stop");
     setNumber(number + 1);
@@ -22,11 +38,26 @@ export default function Slider() {
   const prevbtnHandler = () => {
     setNumber(number - 1);
   };
+  const handleOnMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
+    console.log(e.clientX);
+    if (startX !== null) {
+      const currentX = e.clientX;
+      const deltaX = currentX - startX;
+      const deltaXPercentage = Math.ceil((deltaX - widthRoot) * 100);
+
+      setTranslateX(deltaXPercentage);
+      const limitedTranslateX = Math.min(Math.max(deltaXPercentage, -25));
+      setTranslateX(limitedTranslateX);
+    }
+  };
+
   return (
     <div className=" ">
       <div
         className=" flex gap-10 overflow-hidden
       shrink-0 grow-0    "
+        onMouseMove={handleOnMouseMove}
+        ref={itemRef}
       >
         {works.map((work) => (
           <Image
@@ -42,10 +73,20 @@ export default function Slider() {
         ))}
       </div>
       <div className=" mt-10 space-x-10  ">
-        <button onClick={prevbtnHandler} className="" title="prev">
+        <button
+          type="button"
+          onClick={prevbtnHandler}
+          className=""
+          title="prev"
+        >
           {<Prevbtn />}
         </button>
-        <button onClick={nextbtnHandler} className="" title="next">
+        <button
+          type="button"
+          onClick={nextbtnHandler}
+          className=""
+          title="next"
+        >
           {<Nextbtn />}
         </button>
       </div>
