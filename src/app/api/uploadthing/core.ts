553d6483409d49fc4a290badafec204739420187
shkarDiary <95171810+shkarDiary@users.noreply.github.com>
2024-05-18 14:10:52 +0300

    asads

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -4,7 +4,7 @@ import { UploadThingError } from "uploadthing/server";
 const f = createUploadthing();
 
 const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
+export const dynamic = "force-dynamic";
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
